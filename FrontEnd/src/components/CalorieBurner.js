94fc40e708b5aa37d1c279209666164464974236
mrhiyoko <mgorzkowski@sigma.ug.edu.pl>
2018-12-05 22:40:31 +0100

    new main page

--- FrontEnd/src/components/CalorieBurner.js
+++ FrontEnd/src/components/CalorieBurner.js
@@ -4,23 +4,8 @@ import {Component} from 'react';
 export default class CalorieBurner extends Component{
     render(){
         return(
-            <div id="calorie-burner" className="col-md-10 col-xs-12">
-                <a>
-                    <img className="run" src="src/img/run.jpg"/>
-                </a>
-                <div className="content-list">
-                    <h4>Twój trening</h4>
-                    <ul>
-                        <li>rozgrzewka</li>
-                        <li>10 pompek</li>
-                        <li>20 przysiadów</li>
-                        <li>20 brzuszków</li>
-                        <li>minuta planka</li>
-                        <li>10 martwych ciągów</li>
-                        <li>10 barpies</li>
-                        <li>5 military push</li>
-                    </ul>
-                </div>
+            <div id="calorie-burner" className="col-lg-12 col-md-12 col-xs-12">
+               <h1>STAY FIT</h1>
             </div>
         );
     }
--- FrontEnd/src/components/favorite_food.js
+++ FrontEnd/src/components/favorite_food.js
@@ -4,23 +4,8 @@ import {Component} from 'react';
 export default class FavoriteFood extends Component{
     render(){
         return(
-            <div id="favorite-food" className="col-md-10 col-xs-12">
-                <a>
-                    <img src="src/img/losos.jpg"/>
-                </a>
-                <div className="content-list">
-                    <h4>Twoje ulubione jedzenie</h4>
-                    <ul>
-                        <li>2 filety z łososia ze skórą (każdy po ok 300g)</li>
-                        <li>450g liści szpinaku mrożonego</li>
-                        <li>150g sera sałatkowo-kanapkowego</li>
-                        <li>2 ząbki czosnku</li>
-                        <li>pieprz cytrynowy</li>
-                        <li>sól</li>
-                        <li>sok z cytryny</li>
-                        <li>suszone pomidory</li>
-                    </ul>
-                </div>
+            <div id="favorite-food" className="col-lg-12 col-md-12 col-xs-12">
+               <h1>EAT DELICIOUS AND HEALTHY FOOD</h1>
             </div>
         );
     }
