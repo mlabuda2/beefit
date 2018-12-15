f6103a621043948c8e4fa14245ed5b5558480c13
Wojciech Januszek <wojsamjan@gmail.com>
2018-12-15 19:24:45 +0100

    Added basic routing

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -2,14 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import promise from "redux-promise";
 
-import App from './components/app';
 import reducers from './reducers';
+import App from './components/app';
+import RegisterUser from './components/register_user'
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container-fluid'));
+  <BrowserRouter>
+    <div>
+      <Switch>
+        <Route path="/register/" component={RegisterUser} />
+        <Route path="/" component={App} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+  </Provider>,
+  document.querySelector('.container-fluid'));
