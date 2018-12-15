6a88d8b2771bdf9448b5cd35408bf24d4a014753
Wojciech Januszek <wojsamjan@gmail.com>
2018-12-15 19:25:14 +0100

    Changed navbar

--- FrontEnd/src/containers/navbar_menu.js
+++ FrontEnd/src/containers/navbar_menu.js
@@ -6,7 +6,12 @@ export default class NavbarMenu extends Component{
     render(){
         return(
             <nav className="navbar navbar-light bg-light">
+              {/* <a className="navbar-brand" href="#">Navbar</a> */}
+              {/* <a className="navbar-brand" href="/register/">Register</a> */}
+              <div>
                 <a className="navbar-brand" href="#">Navbar</a>
+                <a className="navbar-brand" href="/register/">Register</a>
+              </div>
             </nav>
         );
     }
