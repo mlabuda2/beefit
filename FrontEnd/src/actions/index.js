7e3c068bb75dc4905fe35f5d772ad8079dd77744
Wojciech Januszek <wojsamjan@gmail.com>
2018-12-16 16:24:22 +0100

    Added full functionality to log in users

--- FrontEnd/src/actions/index.js
+++ FrontEnd/src/actions/index.js
@@ -2,12 +2,23 @@ import axios from "axios";
 
 export const LOGIN_USER = "login_user";
 
-const ROOT_URL = "http://to.do";
+const ROOT_URL = "http://127.0.0.1:5000";
 
 export function loginUser(values, callback) {
   const request = axios
-    // .post(`${ROOT_URL}/todo`, values)
-    .get("https://reqres.in/api/users?page=1")  // sample json api call
+    // .get("https://reqres.in/api/users?page=1")  // sample json api call
+    // .get(`${ROOT_URL}/login`, values)
+    // .get(`${ROOT_URL}/login`,  {headers: {Authorization : "mati:mati"}})
+    // .get(`${ROOT_URL}/login`,  {headers: "mati:mati"})
+    .get(`${ROOT_URL}/login`, {
+        method: 'GET',
+        mode: 'cors',
+        headers: { 'Access-Control-Allow-Origin': true },
+        auth: {
+          username: 'mati',
+          password: 'mati'
+        }
+    })
     .then(() => callback());
 
   return {
