e0801d3da75539a295b239cf6ef05d5a08b7beae
mrhiyoko <mgorzkowski@sigma.ug.edu.pl>
2018-12-24 19:49:06 +0100

    front page new look

--- FrontEnd/src/components/start_with_us.js
+++ FrontEnd/src/components/start_with_us.js
@@ -4,7 +4,7 @@ import {Component} from 'react';
 export default class StartWithUs extends Component{
     render(){
         return(
-            <div id="start-with-us" className="col-lg-12 col-md-12 col-xs-12">
+            <div id="start-with-us" className="col-lg-10 col-md-10 col-xs-12 offset-lg-1 offset-md-1">
                 <h1>START WITH US</h1>
             </div>
         );
