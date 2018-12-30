3c4ab4d72b9ad05179f397067ca7d8066c5ad4d1
mlabuda2 <labuda1996@wp.pl>
2018-12-30 23:29:17 +0100

    insertor working

--- backend/insert_products.py
+++ backend/insert_products.py
@@ -1,7 +1,28 @@
 import csv
+import sqlalchemy
+from api import db
+from models import User, FoodItem, DietPlan, DietPlanUser, DietPlanFoodItem
+
+
+db.drop_all()
+db.create_all()
 
 with open('TABELA_WARTOSCI_ODZYWCZYCH.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     print("PRODUKT KALORIE BIAŁKO TŁUSZCZE WĘGLOWODANY w 100g")
     for row in reader:
-        print(row[0], row[1], row[2], row[3])
+        print(row[0].replace('.', ''), row[1].replace(' kcal', ''), row[2].replace(' g', ''), row[3].replace(' g', ''), row[4].replace(' g', ''))
+        try:
+            item = FoodItem(
+                    name = row[0].replace('.', ''),
+                    calories = int(row[1].replace(' kcal', '')),
+                    protein = row[2].replace(' g', ''),
+                    fat = row[3].replace(' g', ''),
+                    carbs = row[4].replace(' g', '')
+                    )
+            db.session.add(item)
+            db.session.commit()
+        except sqlalchemy.exc.IntegrityError:
+            db.session.rollback()
+            continue
+db.session.close()
