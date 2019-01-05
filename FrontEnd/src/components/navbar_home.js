22cd2c137326d8fc3aa270fd4329ab43d42f70da
Wojciech Januszek <wojsamjan@gmail.com>
2019-01-05 17:29:08 +0100

    Updated NavbarHome

--- FrontEnd/src/components/navbar_home.js
+++ FrontEnd/src/components/navbar_home.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import LogoutUser from "../components/logout_user";
+import DietPlan from "../components/diet_plan";
 
 export default class NavbarHome extends Component{
 
     render(){
         return(
-          <LogoutUser />
+          <nav className="navbar navbar-light bg-light">
+            <div>
+              <a className="navbar-brand" href="/home/diet-plans">Diet Plans</a>
+            </div>
+            <LogoutUser />
+          </nav>
         );
     }
 
