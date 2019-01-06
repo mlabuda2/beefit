ee3f032a52cbbc1a61593ebee54f30e1ce0b02e3
Wojciech Januszek <wojsamjan@gmail.com>
2019-01-06 20:16:07 +0100

    Fixed reducer_diet_plans.js

--- FrontEnd/src/reducers/reducer_diet_plans.js
+++ FrontEnd/src/reducers/reducer_diet_plans.js
@@ -1,10 +1,10 @@
 import _ from "lodash";
 import { FETCH_DIET_PLANS } from "../actions";
 
-export default function(state = [], action) {
+export default function(state = {}, action) {
   switch (action.type) {
     case FETCH_DIET_PLANS:
-      return [action.payload.plan_details, ...state];
+      return action.payload.data.my_diet_plans;
     default:
       return state;
   }
