e8bc23082a1ac37345583bee9f8d92e7e4880db3
Wojciech Januszek <wojsamjan@gmail.com>
2019-01-06 15:16:40 +0100

    Added fetchDietPlans action creator

--- FrontEnd/src/actions/index.js
+++ FrontEnd/src/actions/index.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const LOGIN_USER = "login_user";
 export const REGISTER_USER = "register_user";
+export const FETCH_DIET_PLANS = "fetch_diet_plans";
 
 const ROOT_URL = "http://127.0.0.1:5000";
 
@@ -39,23 +40,32 @@ export function registerUser(values, callback) {
   };
 }
 
-export function isAuthenticated(success_callback, error_callback) {
-  let token = localStorage.getItem('t8k3n');
-  if (!token) {
-    token = "token does not exist";
-  }
-  const request = axios
-    .get(`${ROOT_URL}/is_auth`, {
-        method: 'GET',
-        mode: 'cors',
-        headers: { 'x-access-token': token },
-    })
-    .then((response) => success_callback(response))
-    // .then((response) => console.log(response.data['message']))
-    .catch((error) => error_callback(error));
-    // .catch((error) => console.log(error.response.data['message']));
+export function fetchDietPlans() {
+  const request = axios.get(`${ROOT_URL}/user_plans`);
+
   return {
-    type: LOGIN_USER,
+    type: FETCH_DIET_PLANS,
     payload: request
   };
 }
+
+// export function isAuthenticated(success_callback, error_callback) {
+//   let token = localStorage.getItem('t8k3n');
+//   if (!token) {
+//     token = "token does not exist";
+//   }
+//   const request = axios
+//     .get(`${ROOT_URL}/is_auth`, {
+//         method: 'GET',
+//         mode: 'cors',
+//         headers: { 'x-access-token': token },
+//     })
+//     .then((response) => success_callback(response))
+//     // .then((response) => console.log(response.data['message']))
+//     .catch((error) => error_callback(error));
+//     // .catch((error) => console.log(error.response.data['message']));
+//   return {
+//     type: LOGIN_USER,
+//     payload: request
+//   };
+// }
