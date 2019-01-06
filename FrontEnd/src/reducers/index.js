aa6041aef86f896bec32a44b0f51b1789f9f59cf
Wojciech Januszek <wojsamjan@gmail.com>
2019-01-06 14:04:52 +0100

    Added ProductsReducer

--- FrontEnd/src/reducers/index.js
+++ FrontEnd/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from "redux-form";
+import ProductsReducer from "./reducer_products";
 
 const rootReducer = combineReducers({
+  products: ProductsReducer,
   form: formReducer
 });
 
---
+++ FrontEnd/src/reducers/reducer_products.js
@@ -0,0 +1,7 @@
+export default function() {
+  return [
+    { name: "kurczak", kcal: 99, b: 21.5, t: 1.3, w: 0.0 },
+    { name: "ryż", kcal: 353, b: 7.0, t: 1.0, w: 79.0 },
+    { name: "pomidor", kcal: 19, b: 0.9, t: 0.2, w: 2.9 }
+  ];
+}
