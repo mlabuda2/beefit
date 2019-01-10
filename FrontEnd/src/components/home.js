18f862608065a3776163b965963afae95bef8562
sbilikiewicz <sbilikiewicz@sigma.ug.edu.pl>
2019-01-10 11:10:56 +0100

    Update home.js

--- FrontEnd/src/components/home.js
+++ FrontEnd/src/components/home.js
@@ -3,12 +3,14 @@ import NavbarHome from "./navbar_home";
 import FooterMenu from "./footer_menu";
 import BMR from "./BMR";
 import BMR2 from "./BMR2";
+import BMI from "./BMI";
 
 export default class Home extends Component {
   render() {
     return (
       <div>
         <NavbarHome />
+            <BMI/>
             <BMR/>
             <BMR2/>
         <FooterMenu />
