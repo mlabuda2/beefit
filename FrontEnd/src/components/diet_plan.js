3609265f9ab8ad873831651fc6f885c62feca3ba
Wojciech Januszek <wojsamjan@gmail.com>
2019-01-10 21:03:53 +0100

    Updated DietPlanList, DietPlan

--- FrontEnd/src/components/diet_plan.js
+++ FrontEnd/src/components/diet_plan.js
@@ -1,12 +1,39 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { fetchDietPlans } from "../actions"
 
 class DietPlan extends Component {
+  componentDidMount() {
+    const { diet_plans } = this.props;
+    // if (diet_plans === null || diet_plans.length == 0) {
+    if (Object.keys(diet_plans).length == 0) {
+      this.props.fetchDietPlans();
+      console.log("DietPlan - componentDidMount: " + diet_plans);
+    }
+  }
+
   render() {
-    return (
+    const { diet_plan, diet_plans } = this.props;
+    console.log("render:" + diet_plan)
+    if (!diet_plan) {
+      return (
         <div></div>
+      );
+    }
+    return (
+      <div>
+        <button type="submit" className="btn btn-primary" onClick={() => this.props.history.push("/home/diet-plans")}>Back</button>
+        {diet_plan.name}
+      </div>
     );
   }
 }
 
-export default DietPlan;
+function mapStateToProps({ diet_plans }, ownProps) {
+  return {
+    diet_plan: diet_plans[ownProps.match.params.id],
+    diet_plans: diet_plans
+  };
+}
+
+export default connect(mapStateToProps, { fetchDietPlans })(DietPlan);
