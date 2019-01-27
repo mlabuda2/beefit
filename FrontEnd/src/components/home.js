939bd452e543210da88f8defedc72a9af9687ee1
sbilikiewicz <sbilikiewicz@sigma.ug.edu.pl>
2019-01-27 13:08:24 +0100

    Update home.js

--- FrontEnd/src/components/home.js
+++ FrontEnd/src/components/home.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import NavbarHome from "./navbar_home";
 import BMR from "./BMR";
-import BMR2 from "./BMR2";
 import BMI from "./BMI";
 
 export default class Home extends Component {
@@ -9,9 +8,8 @@ export default class Home extends Component {
     return (
       <div>
         <NavbarHome />
-        <BMR/>
-        <BMR2/>
         <BMI/>
+        <BMR/>
         <br/><br/><br/><br/><br/><br/>
       </div>
     );
